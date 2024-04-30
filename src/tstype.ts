a966f56b93f2fae88d19b536239fb6178ca82178
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-04-30 21:55:14 +0600

    define types

---
+++ src/tstype.ts
@@ -0,0 +1,20 @@
+// peimetive data
+// string
+let myName:string ="taz";
+//number
+let roll:number=925;
+//boolean
+let isAdmin:boolean=true;
+// undefine
+let undefineValue:undefined=undefined;
+// null
+let janiNa:null=null;
+
+//array
+
+let student:string[]=["taz","raz","jony"];
+let rollCollection:number[]=[20,52,45];
+
+//tuple
+
+let nameAge:[string,number,boolean]=["taz",25,true];
