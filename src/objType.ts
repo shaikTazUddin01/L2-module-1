511dcf354e08fdf636a4105c8287214343724743
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-04-30 22:17:31 +0600

    ts object type

---
+++ src/objType.ts
@@ -0,0 +1,15 @@
+// object types
+
+const user:{
+   readonly company:string;
+    Fname:string;
+    mname?:string;
+    lname:string;
+    isMarred:boolean
+} ={
+    company:"programming hero",
+    Fname:"taz",
+    lname:"uddin",
+    isMarred:true
+}
+// user.company="bangladesh"
