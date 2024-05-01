b3d4f97c3c7124db692589db870357e09d78dda9
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-05-01 13:35:10 +0600

    tarnary opator & nullish

---
+++ src/tarnary.ts
@@ -0,0 +1,14 @@
+{
+    // const age=19;
+    // if(age>18){
+    //     console.log("adult");
+    // }else{
+    //     console.log("not audlt");
+    // }
+    // const isAdult = age>=18 ?"adult":"not adult";
+    // console.log({isAdult});
+
+    const isAdult = null;
+    const result = isAdult ?? "ghust"
+    console.log({ result });
+}
